f5a981d122d4c433fd17581137ef22e1c4e6721c
habr0403 <brandonhalloran@yahoo.com>
2025-02-24 01:28:26 -0600

    commit

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('.box').hover(
+        function() {
+            
+            $(this).animate({
+                width: '200px',
+                height: '200px',
+                opacity: 1
+            }, 800, 'easeOutBounce'); 
+        },
+        function() {
+            
+            $(this).animate({
+                width: '100px',
+                height: '100px',
+                opacity: 0.8
+            }, 800, 'easeInElastic'); 
+        }
+    );
+});
